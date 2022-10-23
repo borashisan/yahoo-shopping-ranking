69d5b5115d1bde882bcefa6760503b4bb4406cb6
borashisan <tomochuro731@gmail.com>
2022-10-23 17:09:02 +0900

    connected-react-routerと関連packageのバージョンを調整

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import createStore from './createStore';
+
+const history = createBrowserHistory();
+
+const store = createStore(history);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
